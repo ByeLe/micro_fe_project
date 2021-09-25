36c510d5b0916ab18dfadf8fd76128490082afad
Mac <mac@192.168.0.102>
2021-09-25 17:00:24 +0800

    init

---
+++ class3/qiankun_project/src/micro-app.js
@@ -0,0 +1,22 @@
+const microApps = [
+    {
+      name: 'micro_vue',
+      entry: '//localhost:8081/',
+      activeRule: '/micro_vue',
+      container: '#subapp-viewport', // 子应用挂载的div
+      props: {
+        routerBase: '/micro_vue' // 下发路由给子应用，子应用根据该值去定义qiankun环境下的路由
+      }
+    },
+    {
+      name: 'micro_react',
+      entry: '//localhost:10100/',
+      activeRule: '/micro_react',
+      container: '#subapp-viewport', // 子应用挂载的div
+      props: {
+        routerBase: '/micro_react'
+      }
+    }
+  ]
+  
+  export default microApps
---
+++ class3/qiankun_react/src/List/index.jsx
@@ -0,0 +1,19 @@
+function List() {
+    const data = [
+        {
+            id: 1,
+            name: '123',
+        },
+        {
+            id: 2,
+            name: '456',
+        },
+    ];
+    const content = (
+        <>
+        <div>我是list界面---</div>
+        </>
+    )
+    return content;
+}; 
+export default List;
